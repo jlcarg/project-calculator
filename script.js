6091b30707ac0efd1ce738af7237d7cb5590d394
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-05-01 16:32:16 +0900

    Build the html representation of the calculator
    Create the buttons via javascript and add first version of styling via css.

--- script.js
+++ script.js
@@ -1,6 +1,77 @@
-container = document.querySelector(".container");
+const keypad = document.querySelector(".keypad");
+const buttonsChars = ['C', '⌫'
+                      , 'addition', '7', '8', '9'
+                      , 'subtraction', '4', '5', '6'
+                      , 'multiplication', '1', '2', '3'
+                      , 'division', '0', 'dot', 'equal']
 
-newDiv = document.createElement('div');
-newDiv.textContent = "hello";
+for (let button of buttonsChars) {
+    const newButton = document.createElement('button');
+    newButton.classList.add('button', button);
+    newButton.textContent = button;
+    keypad.appendChild(newButton);
 
-container.appendChild(newDiv);
+    switch (button) {
+        case "addition":
+            newButton.textContent = "+";
+            break;
+        case "subtraction":
+            newButton.textContent = "-";
+            break;
+        case "multiplication":
+            newButton.textContent = "*";
+            break;
+        case "division":
+            newButton.textContent = "/";
+            break;
+        case "dot":
+            newButton.textContent = ".";
+            break;
+        case "equal":
+            newButton.textContent = "=";
+            break;
+    }
+
+}
+
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    
+    let operation;
+    switch (operator) {
+        case "+":
+            operation = add;
+            break;
+        case "-":
+            operation = subtract;
+            break;
+        case "*":
+            operation = multiply;
+            break;
+        case "/":
+            operation = divide;
+            break;
+    }
+
+    return operation(num1, num2);
+
+}
