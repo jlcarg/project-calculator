1f08bfc3263d6a49d552afd16128e730affe5afb
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-05-01 19:26:57 +0900

    Add first draft of the project logic
    Add event listeners to the buttons
    Add logic to call operate function (With severe issues to solve regarding manipulation of display and variables)
    Display the returned value from function operate

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const display = document.querySelector('.display');
 const displayedDigits = document.createElement('div');
 displayedDigits.classList.add('digits');
 display.appendChild(displayedDigits);
-displayedDigits.textContent = "123000000000000000000000000000"
+displayedDigits.textContent = "0";
 
 const keypad = document.querySelector('.keypad');
 const buttonsChars = ['C', '⌫'
-                      , 'addition', '7', '8', '9'
-                      , 'subtraction', '4', '5', '6'
-                      , 'multiplication', '1', '2', '3'
-                      , 'division', '0', 'dot', 'equal']
+                      , 'add', 7, 8, 9
+                      , 'subtract', 4, 5, 6
+                      , 'multiply', 1, 2, 3
+                      , 'divide', 0, 'dot', 'equal']
 
 for (let button of buttonsChars) {
     const newButton = document.createElement('button');
@@ -18,16 +18,16 @@ for (let button of buttonsChars) {
     keypad.appendChild(newButton);
 
     switch (button) {
-        case "addition":
+        case "add":
             newButton.textContent = '+';
             break;
-        case "subtraction":
+        case "subtract":
             newButton.textContent = '-';
             break;
-        case "multiplication":
+        case "multiply":
             newButton.textContent = '*';
             break;
-        case "division":
+        case "divide":
             newButton.textContent = '/';
             break;
         case "dot":
@@ -40,10 +40,53 @@ for (let button of buttonsChars) {
 
 }
 
+const buttonsArr = Array.from(document.querySelectorAll('.button'));
+const buttonsFromOneToNine = buttonsArr
+                                .filter(button => Number.isInteger(
+                                                  Number.parseInt(button.textContent)) && 
+                                                  Number.parseInt(button.textContent) !=  0);
+const buttonZero = buttonsArr.find(button => button.textContent == '0');
+const buttonsOperators = buttonsArr.filter(button => button.textContent == '+' 
+                                                  || button.textContent == '-' 
+                                                  || button.textContent == '*' 
+                                                  || button.textContent == '/')
+const buttonEqual = buttonsArr.find(button => button.textContent == '=');
+const buttonDot = buttonsArr.find(button => button.textContent == '.');
+const buttonC = buttonsArr.find(button => button.textContent == 'C');
+const buttonBackspace = buttonsArr.find(button => button.textContent == '⌫');
 
-let number1 = 0;
-let number2 = 0;
+buttonsFromOneToNine.forEach(button => button.addEventListener('click', event => {
+                                                    if (displayedDigits.textContent == '0') {
+                                                        displayedDigits.textContent = event.target.textContent
+                                                    } else {
+                                                    displayedDigits.textContent += event.target.textContent}
+}))
+
+buttonZero.addEventListener('click', event => {
+    if (displayedDigits.textContent == '0') {
+        return;
+    } else { displayedDigits.textContent += event.target.textContent}
+})
+
 let operator = '';
+let num1 = 0;
+let num2 = 0;
+
+buttonsOperators.forEach(button => button.addEventListener('click', event => {
+    if (operator.length === 0) {
+        operator = event.target.textContent;
+        num1 = Number.parseInt(displayedDigits.textContent)};
+        displayedDigits.textContent = "0";
+    
+}))
+
+buttonEqual.addEventListener('click', () => {
+    num2 = Number.parseInt(displayedDigits.textContent);
+    displayedDigits.textContent = operate(num1, num2, operator);
+    operator = '';
+    num1 = displayedDigits.textContent;
+    num2 = 0;
+})
 
 function add(num1, num2) {
     return num1 + num2;
